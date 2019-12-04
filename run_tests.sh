a8d39327815a727f20e542efea5ba85702d6df05
drons <drons@list.ru>
2019-12-05 00:48:43 +0300

    Local coverage run

--- run_tests.sh
+++ run_tests.sh
@@ -3,15 +3,15 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bot/local/gdal301/lib:/home/bot/local/proj611/lib:/home/bot/local/openjpeg-2.3.0/lib
 source venv/bin/activate
 
-python world-dl.py -h
+coverage run world-dl.py -h
 
-python world-dl.py -a init \
+coverage run --append world-dl.py -a init \
 -s 65536 -b 2048 \
 -i ./input/google_map.img \
 -o ./out \
 -m ./data/mask-no-ant-3857.tif
 
-python world-dl.py -a download \
+coverage run --append world-dl.py -a download \
 -ov -u -c LZMA -t 512 \
 -i ./input/google_map.img \
 -o ./out
@@ -20,16 +20,19 @@ rm ./out/gmap_0_0.tif
 rm ./out/gmap_0_2048.tif
 echo "Hello" >> ./out/gmap_2048_2048.tif
 
-python world-dl.py -a merge -v \
+coverage run --append world-dl.py -a merge -v \
 -i ./input/google_map.img \
 -o ./out || true && false
 
-python world-dl.py -a download \
+coverage run --append world-dl.py -a download \
 -ov -u -c LZMA -t 512 \
 -i ./input/google_map.img \
 -o ./out
 
-python world-dl.py -a merge -v \
+coverage run --append world-dl.py -a merge -v \
 -i ./input/google_map.img \
 -o ./out
+
+coverage report --include world-dl.py
+coverage html --include world-dl.py
 
